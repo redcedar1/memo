746f8d26003591e9952127bdbbf9190c0e401082
ki <redcedar1@naver.com>
2023-07-29 13:29:15 +0900

    init commit

---
+++ src/main/java/com/sparta/memo06/Memo06Application.java
@@ -0,0 +1,13 @@
+package com.sparta.memo06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Memo06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Memo06Application.class, args);
+    }
+
+}
