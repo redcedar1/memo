888da8b197a20035652811fff3a9aac1b215535e
ki <redcedar1@naver.com>
2023-07-29 15:05:15 +0900

    Create, Read 구현

---
+++ src/main/java/com/sparta/memo06/controller/MemoController.java
@@ -0,0 +1,39 @@
+package com.sparta.memo06.controller;
+
+import com.sparta.memo06.dto.MemoRequestDto;
+import com.sparta.memo06.dto.MemoResponseDto;
+import com.sparta.memo06.entity.Memo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
+    @PostMapping("/memos")
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto){
+        Memo memo = new Memo(requestDto);
+
+        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
+        memo.setId(maxId);
+
+        memoList.put(memo.getId(), memo);
+
+        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+
+        return memoResponseDto;
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos(){
+        List<MemoResponseDto> responseList = memoList.values().stream()
+                .map(MemoResponseDto::new).toList();
+
+        return responseList;
+    }
+}
---
+++ src/main/java/com/sparta/memo06/dto/MemoRequestDto.java
@@ -0,0 +1,9 @@
+package com.sparta.memo06.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String contents;
+}
